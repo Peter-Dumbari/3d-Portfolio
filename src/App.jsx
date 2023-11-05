dfa0b2fb9836f7c2f3e2406ad919420d0239c3ee
Peter Dumbari <dummytwiz@gmail.com>
2023-11-05 17:07:44 +0100

    setup the app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
@@ -7,6 +20,15 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
