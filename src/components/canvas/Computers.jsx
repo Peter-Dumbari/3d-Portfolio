3df0b02003261fec9c4860b57cf2b219bd6a7dd5
Peter Dumbari <dummytwiz@gmail.com>
2025-09-15 14:06:45 +0100

    hides canvas on mobile screen for trouble shooting sake

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -18,12 +18,14 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={3} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.6 : 0.75}
-        position={isMobile ? [0, -2, -1.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
+      {!isMobile && (
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.6 : 0.75}
+          position={isMobile ? [0, -2, -1.2] : [0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      )}
     </mesh>
   );
 };
